73f53846cc68d857642ad9e67f0c9ad5b896b1f7
cuizhimin <inocent@foxmail.com>
2016-06-27 11:55:01 +0800

    new calculator
    编辑一个简易计算器，计算器可以进行加减乘除四则运算，可以对输入的数字进行重置清零，添加一个编辑框显示输入输出时的结果。

--- new calculator/ViewController.swift
+++ new calculator/ViewController.swift
@@ -6,20 +6,259 @@
 //  Copyright © 2016年 czm. All rights reserved.
 //
 
+
+
 import UIKit
-
-class ViewController: UIViewController {
-
+class ViewController: UIViewController{
+    
+    @IBOutlet weak var Text: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    var flag:Int=0;
+    var num1:Double=0;
+    var num2:Double=0;
+    var Equal:Double=0;
+    var judgepoint:Bool=false;
+    
+    
+    @IBAction func one(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"1"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"1";
+        }
+    }
+    @IBAction func two(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"2"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"2";
+        }
+    }
+    @IBAction func three(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"3"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"3";
+        }
+    }
+    
+    @IBAction func four(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"4"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"4";
+        }
+    }
+    @IBAction func five(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"5"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"5";
+        }
+    }
+    @IBAction func six(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"6"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"6";
+        }
+    }
+    @IBAction func seven(sender: AnyObject) {
+        if( Text.text=="0")
+        {
+            Text.text=""
+            Text.text=(Text.text)!+"7"
+            
+        }
+        else
+        {
+            Text.text=(Text.text)!+"7";
+        }
+    }
+    @IBAction func eight(sender: AnyObject) {        if( Text.text=="0")
+    {
+        Text.text=""
+        Text.text=(Text.text)!+"8"
+        
+    }
+    else
+    {
+        Text.text=(Text.text)!+"8";
+        }
+    }
+    @IBAction func nine(sender: AnyObject) {        if( Text.text=="0")
+    {
+        Text.text=""
+        Text.text=(Text.text)!+"9"
+        
+    }
+    else
+    {
+        Text.text=(Text.text)!+"9";
+        }
+    }
+    @IBAction func zero(sender: AnyObject) {        if( Text.text=="0")
+    {
+        Text.text=""
+        Text.text=(Text.text)!+"0"
+        
+    }
+    else
+    {
+        Text.text=(Text.text)!+"0";
+        }
+    }
+    @IBAction func clean(sender: AnyObject) {Text.text="0";
+        num1=0;
+        num2=0;
+        Equal=0;
+        judgepoint=false
 
+    }
 
+    @IBAction func dengyu(sender: AnyObject) {var x:Int=0;
+        num2=((Text.text)! as NSString).doubleValue;
+        switch(flag)
+        {
+        case 1:
+            
+            Equal=num1+num2;
+            if(Equal%1==0)
+            {
+                x=Int(Equal);
+                Text.text="\(x)"
+            }
+            else
+            {
+                Text.text="\(Equal)"
+            }
+            judgepoint=true;
+            break;
+        case 2:
+            
+            Equal=num1-num2;
+            if(Equal%1==0)
+            {
+                x=Int(Equal);
+                Text.text="\(x)"
+            }
+            else
+            {
+                Text.text="\(Equal)"
+            }
+            judgepoint=true;
+            break;
+        case 3:
+            
+            Equal=num1*num2;
+            if(Equal%1==0)
+            {
+                x=Int(Equal);
+                Text.text="\(x)"
+            }
+            else
+            {
+                Text.text="\(Equal)"
+            }
+            judgepoint=true;
+            break;
+        case 4:
+            if(num2==0)
+            {
+                Text.text="错误"
+            }
+            else
+            {
+                Equal=num1/num2;
+                if(Equal%1==0)
+                {
+                    x=Int(Equal);
+                    Text.text="\(x)"
+                }
+                else
+                {
+                    Text.text="\(Equal)"
+                }
+            }
+            judgepoint=true;
+            break;
+        default:
+            break;
+        }
+    }
+      
+    @IBAction func point(sender: AnyObject) {
+        if(judgepoint==false)
+        {
+            Text.text=(Text.text)!+".";
+            judgepoint=true
+        }
+    }
+    @IBAction func add(sender: AnyObject) {
+        num1=((Text.text)! as NSString).doubleValue;
+        Text.text="";
+        flag=1;
+        judgepoint=false;
+
+    }
+    @IBAction func jian(sender: AnyObject) {
+        num1=((Text.text)! as NSString).doubleValue;
+        Text.text="";
+        flag=2;
+        judgepoint=false;
+    }
+    @IBAction func cheng(sender: AnyObject) {
+        num1=((Text.text)! as NSString).doubleValue;
+        Text.text="";
+        flag=2;
+        judgepoint=false;
+        
+
+    }
+    @IBAction func chu(sender: AnyObject) {
+        num1=((Text.text)! as NSString).doubleValue;
+        Text.text="";
+        flag=4;
+        judgepoint=false;
+
+    }
 }
-
